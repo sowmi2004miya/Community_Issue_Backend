2c84e05c9dd61f9bcad8783b187c3ccf139ae63d
sowmi2004miya <g.sowmiya2004@gmail.com>
2025-10-31 11:49:23 +0530

    changes cleared

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const startServer = async () => {
   // Start Server
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () =>
-    console.log(`🚀 Server running on http://localhost:${PORT}`)
+    console.log(`🚀 Server running on https://community-issue-backend.onrender.com:${PORT}`)
   );
 };
 
